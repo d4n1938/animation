e918629e868dc60abb8c98c4159986199b496d7e
Lnida <daniksb@icloud.com>
2022-09-20 17:50:26 +0900

    click ani

--- pages/Ani04.tsx
+++ pages/Ani04.tsx
@@ -1,23 +1,23 @@
-import anime from "../anime/lib/anime.es.js";
-// import anime from "animejs";
-
-// const anime = require("animejs");
-anime({
-  targets: "div",
-  translateX: 250,
-});
+import { motion, useCycle } from "framer-motion";
 const Ani04 = () => {
+  const [x, cyclex] = useCycle(0, 50, 100, 150);
+
   return (
     <>
       <section>
-        <div>animate</div>
+        <motion.div
+          style={{ width: "200px", height: "200px", background: "skyblue" }}
+          animate={{ x: x }}
+          onClick={() => cyclex()}
+        />
       </section>
+
       {/* style--------------------------------------------------- */}
       <style jsx>{`
         @import "../styles/functions.scss";
         section {
           @include flex-center("", 600px);
-          background-color: rgb(255, 0, 0);
+          background-color: rgb(223, 223, 223);
         }
       `}</style>
     </>
