d5de5cbc41169c61755a3a66789a3045130f66c5
Lnida <daniksb@icloud.com>
2022-10-05 23:16:12 +0900

    add ani09

--- pages/Ani09.tsx
+++ pages/Ani09.tsx
@@ -1,9 +1,38 @@
+import gsap from "gsap";
+import { useEffect } from "react";
+
 const Ani09 = () => {
+  useEffect(() => {
+    gsap.to(".cube", {
+      x: 100,
+      duration: 1,
+    });
+  });
   return (
     <>
-      <section></section>
+      <section>
+        <div className="cube"></div>
+      </section>
       {/* style--------------------------------------------------- */}
-      <style jsx>{``}</style>
+      <style jsx>{`
+        @import "../styles/functions.scss";
+
+        section {
+          @include flex-center(100vw, 100vh);
+          font-size: 3rem;
+
+          background-color: rgb(11, 11, 11);
+          position: fixed;
+          top: 0;
+          left: 0;
+          z-index: 2000;
+          .cube {
+            @include flex-center(100px, 100px);
+
+            background-color: aliceblue;
+          }
+        }
+      `}</style>
     </>
   );
 };
