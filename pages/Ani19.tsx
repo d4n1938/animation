c4294fa7b637119387521df1bfa7282c616340ac
Lnida <91710828+d4n1938@users.noreply.github.com>
2022-10-19 23:38:59 +0900

    add ani19

---
+++ pages/Ani19.tsx
@@ -0,0 +1,16 @@
+const Ani19 = () => {
+  return (
+    <>
+      <section></section>
+      {/* style--------------------------------------------------- */}
+      <style jsx>{`
+        @import "../styles/functions.scss";
+
+        section {
+          @include flex-center("", 100vh);
+        }
+      `}</style>
+    </>
+  );
+};
+export default Ani19;
