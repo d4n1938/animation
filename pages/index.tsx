e9080cc1fdff2d9dac5af1a3d613939d1f4ea85c
Lnida <91710828+d4n1938@users.noreply.github.com>
2022-11-29 23:35:19 +0900

    hide nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,13 +20,13 @@ import Navigation from "./Navigation";
 const Home: NextPage = () => {
   return (
     <>
-      <Navigation></Navigation>
+      {/* <Navigation></Navigation> */}
       <AniUnity10></AniUnity10>
       {/* <Ani12></Ani12> */}
       {/* <Ani13></Ani13> */}
       {/* <Ani14></Ani14> */}
       {/* <Ani15></Ani15> */}
-      {/* <Ani16></Ani16> */}
+      <Ani16></Ani16>
       <Ani17></Ani17>
       <Ani18></Ani18>
       <Ani19></Ani19>
